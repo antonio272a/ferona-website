4086a6346de0f9ee6951debcbebbfefa3a6a02ed
antonio272a <antoniofelipe2001@hotmail.com>
2022-06-24 14:52:38 -0300

    up

--- assets/src/script.js
+++ assets/src/script.js
@@ -41,47 +41,45 @@ const baseOptions5 = {
   containerID: "container3d_replace",
 };
 
-const baseOptions6 = {
-  distID: "latest",
-  solution3DName: "sb002-tb",
-  projectName: "first-project",
-  solution3DID: "42545",
-  containerID: "container3d_replace",
-};
 
 
-const options = {
-  ...baseOptions6,
+const loadWebsite = async (solutionName, solutionId) => {
+  
+  const baseOptions6 = {
+    distID: "latest",
+    solution3DName: solutionName,
+    projectName: "first-project",
+    solution3DID: solutionId,
+    containerID: "container3d_replace",
+  };
 
-  onLoadingChanged: function (loading) {
-    loadingBar.style.width = loading.progress + "%";
-  },
+  const options = {
+    ...baseOptions6,
 
-  onCameraInteraction: function () {
-    if (flagInterract) {
-      document.getElementsByClassName("drag-and-scroll")[0].style.display =
-        "none";
+    onLoadingChanged: function (loading) {
+      loadingBar.style.width = loading.progress + "%";
+    },
 
-      Unlimited3D.enableAutoRotate({
-        enable: false,
-      });
+    onCameraInteraction: function () {
+      if (flagInterract) {
+        document.getElementsByClassName("drag-and-scroll")[0].style.display =
+          "none";
 
-      flagInterract = false;
-    }
-  },
+        Unlimited3D.enableAutoRotate({
+          enable: false,
+        });
 
-};
-
-
-
-window.onload = async () => {
+        flagInterract = false;
+      }
+    },
+  };
+  
+  
   const threedium = new Threedium();
   await threedium.init(options);
 
   const initialState = {
-    selectedTop: threedium.parts.find((part) =>
-      part.startsWith("[top](01)")
-    ),
+    selectedTop: threedium.parts.find((part) => part.startsWith("[top](01)")),
     selectedBottom: threedium.parts.find((part) =>
       part.startsWith("[bottom](01)")
     ),
@@ -91,94 +89,112 @@ window.onload = async () => {
     materials: threedium.materials,
     blockedMaterials: [],
   };
-  
+
   const store = new Store(initialState);
-  
-  const topPartContainer = document.getElementById('top-part-container');
-  const bottomPartContainer = document.getElementById('bottom-part-container');
-  const topMaterialContainer = document.getElementById('top-material-container');
-  const bottomMaterialContainer = document.getElementById('bottom-material-container');
+
+  const topPartContainer = document.getElementById("top-part-container");
+  const bottomPartContainer = document.getElementById("bottom-part-container");
+  const topMaterialContainer = document.getElementById(
+    "top-material-container"
+  );
+  const bottomMaterialContainer = document.getElementById(
+    "bottom-material-container"
+  );
 
   const clearButtons = () => {
-    topPartContainer.innerHTML = '';
-    bottomPartContainer.innerHTML = '';
-    topMaterialContainer.innerHTML = '';
-    bottomMaterialContainer.innerHTML = '';
-  }
+    topPartContainer.innerHTML = "";
+    bottomPartContainer.innerHTML = "";
+    topMaterialContainer.innerHTML = "";
+    bottomMaterialContainer.innerHTML = "";
+  };
 
-  const handlePartInput = ({target: { id }}) => {
-    const partType = id.slice(id.indexOf('[') + 1, id.indexOf(']'));
+  const handlePartInput = ({ target: { id } }) => {
+    const partType = id.slice(id.indexOf("[") + 1, id.indexOf("]"));
     const capitalizedPartType = partType.replace(/^\w/, (c) => c.toUpperCase());
-    store.setState({[`selected${capitalizedPartType}`]: id })
-  }
+    store.setState({ [`selected${capitalizedPartType}`]: id });
+  };
 
   const genericCreatePartsButtons = (parts, container, selected) => {
     parts.forEach((name) => {
       const label = document.createElement("label");
       label.htmlFor = name;
       label.innerText = name;
-      
+
       const input = document.createElement("input");
       input.type = "checkbox";
       input.id = name;
       input.onchange = handlePartInput;
-      
+
       if (name === selected) {
         input.checked = true;
         input.disabled = true;
       }
-      
+
       label.appendChild(input);
       container.appendChild(label);
     });
   };
 
   const createPartsButtons = () => {
-    const { state: { parts, selectedTop, selectedBottom } } = store;
-    const topParts = parts.filter((part) => part.startsWith('[top]'));
-    const bottomParts = parts.filter((part) => part.startsWith('[bottom]'));
-    
+    const {
+      state: { parts, selectedTop, selectedBottom },
+    } = store;
+    const topParts = parts.filter((part) => part.startsWith("[top]"));
+    const bottomParts = parts.filter((part) => part.startsWith("[bottom]"));
+
     genericCreatePartsButtons(topParts, topPartContainer, selectedTop);
-    
+
     genericCreatePartsButtons(bottomParts, bottomPartContainer, selectedBottom);
-  }
+  };
 
   const getSubParts = (selectedPart) => {
-    const { state: { parts } } = store;
+    const {
+      state: { parts },
+    } = store;
     const partType = selectedPart.slice(0, selectedPart.indexOf("]") + 1);
-    const partNumber = selectedPart.slice(selectedPart.indexOf('('), selectedPart.indexOf(')') + 1);
+    const partNumber = selectedPart.slice(
+      selectedPart.indexOf("("),
+      selectedPart.indexOf(")") + 1
+    );
 
     subPartsIndex = selectedPart
       .slice(selectedPart.indexOf("{") + 1, selectedPart.indexOf("}"))
       .split("-");
-    
+
     const subParts = parts.filter((part) => {
       const isSubNode = part.startsWith(`[subpart]${partType}${partNumber}`);
-      const isSubPart = subPartsIndex.some((index) => part.startsWith(`${partType}(${index})`))
+      const isSubPart = subPartsIndex.some((index) =>
+        part.startsWith(`${partType}(${index})`)
+      );
       return isSubNode || isSubPart;
     });
 
     return subParts;
-  }
+  };
 
   const createMaterialsButtons = (part, container) => {
-    const { state: { materials } } = store;
+    const {
+      state: { materials },
+    } = store;
     const { applyMaterials } = threedium;
-    const partContainer = document.createElement('div');
+    const partContainer = document.createElement("div");
     partContainer.id = part;
-    partContainer.className = 'border border-danger border-2'
-    
+    partContainer.className = "border border-danger border-2";
+
     const filteredMaterials = materials.filter((material) => {
-      const materialReference = material.slice(material.indexOf('['), material.indexOf(')') + 1);
+      const materialReference = material.slice(
+        material.indexOf("["),
+        material.indexOf(")") + 1
+      );
       const isMaterial = materialReference.startsWith(
         part.slice(0, part.indexOf(")") + 1)
       );
       if (material.match(/{/g)) {
-        const isSubMaterial = (material.match(/{/g).length > 2);
-        return isMaterial && !isSubMaterial
+        const isSubMaterial = material.match(/{/g).length > 2;
+        return isMaterial && !isSubMaterial;
       }
-       
-      return isMaterial
+
+      return isMaterial;
     });
     // console.log(part, filteredMaterials);
     filteredMaterials.forEach((material) => {
@@ -186,25 +202,25 @@ window.onload = async () => {
       // label.htmlFor = material;
       // label.innerText = material;
 
-      const buttton = document.createElement('button');
+      const buttton = document.createElement("button");
       buttton.id = material;
       buttton.innerText = material;
-      buttton.onclick = ({target: { id }}) => applyMaterials(id); 
+      buttton.onclick = ({ target: { id } }) => applyMaterials(id);
       // label.appendChild(input);
       partContainer.appendChild(buttton);
     });
 
     container.appendChild(partContainer);
-  }
+  };
 
   const createMaterialContainers = () => {
     const {
-      state: { selectedTop, selectedBottom},
+      state: { selectedTop, selectedBottom },
     } = store;
 
     const topSubParts = getSubParts(selectedTop);
 
-    if(!selectedTop.includes('#')) {
+    if (!selectedTop.includes("#")) {
       createMaterialsButtons(selectedTop, topMaterialContainer);
     }
 
@@ -213,7 +229,6 @@ window.onload = async () => {
         createMaterialsButtons(subPart, topMaterialContainer);
       }
     });
-    
 
     const bottomSubParts = getSubParts(selectedBottom);
 
@@ -226,14 +241,16 @@ window.onload = async () => {
         createMaterialsButtons(subPart, bottomMaterialContainer);
       }
     });
-  }
+  };
 
   const selectParts = async () => {
     const { selectPart } = threedium;
-    const { state: { selectedTop, selectedBottom } } = store;
+    const {
+      state: { selectedTop, selectedBottom },
+    } = store;
     await selectPart(selectedTop);
     await selectPart(selectedBottom);
-  }
+  };
 
   store.setStateCallback(async () => {
     clearButtons();
@@ -246,98 +263,114 @@ window.onload = async () => {
   //   // console.log(threedium.parts);
   //   // console.log(threedium.materials);
   //   console.log(store.state);
-    
+
   // });
 
   // document.getElementById("change1").addEventListener("click", async () => {
   //   console.log(threedium);
   // });
   await store._stateCallback();
+};
+
+
+window.onload = () => {
+  const idInput = document.getElementById("id-input");
+  const nameInput = document.getElementById("name-input");
+  const testButton = document.getElementById("test-button");
+  const inputSection = document.getElementById("input-section");
+
+  testButton.addEventListener('click', () => {
+    const { value: nameValue } = nameInput;
+    const { value: idValue } = idInput;
+    loadWebsite(nameValue.trim(), idValue.trim());
+    inputSection.innerHTML = '';
+  })
+  
 }
 
 
 
-document.getElementById("change1").addEventListener("click", async () => {
-  Unlimited3D.getAvailableCustomParts(function (error, results) {
-    console.log(results);
-  });
-  // Unlimited3D.showParts({
-  //   parts: ["[top](01):PA001_TC001_Mesh"],
-  // });
-  // Unlimited3D.showParts({
-  //   partObjects: [
-  //     {
-  //       parts: [
-  //         "[top](01):PA001_TC001_Mesh",
-  //         "SA 001:[subpart][top](01){2}#[top](01)",
-  //         "SA 001:[subpart][top](01){1}#[top](01)",
-  //       ],
-  //     },
-  //   ],
-  // });
-  // Unlimited3D.changeMaterialMap(
-  //   { material: "Material01", texture: "Texture01", mapType: "diffuseMap" },
-  //   () => {}
-  // );
+// document.getElementById("change1").addEventListener("click", async () => {
+//   Unlimited3D.getAvailableCustomParts(function (error, results) {
+//     console.log(results);
+//   });
+//   // Unlimited3D.showParts({
+//   //   parts: ["[top](01):PA001_TC001_Mesh"],
+//   // });
+//   // Unlimited3D.showParts({
+//   //   partObjects: [
+//   //     {
+//   //       parts: [
+//   //         "[top](01):PA001_TC001_Mesh",
+//   //         "SA 001:[subpart][top](01){2}#[top](01)",
+//   //         "SA 001:[subpart][top](01){1}#[top](01)",
+//   //       ],
+//   //     },
+//   //   ],
+//   // });
+//   // Unlimited3D.changeMaterialMap(
+//   //   { material: "Material01", texture: "Texture01", mapType: "diffuseMap" },
+//   //   () => {}
+//   // );
   
-  // Unlimited3D.changeMaterial(
-  //   {
-  //     parts: ["TA003 Yellow Fireworks Black and Black:Node-3"],
-  //     material: "TA Black Fireworks",
-  //   },
-  //   (e, r) => {
-  //     console.log(e, r);
-  //   }
-  // );
+//   // Unlimited3D.changeMaterial(
+//   //   {
+//   //     parts: ["TA003 Yellow Fireworks Black and Black:Node-3"],
+//   //     material: "TA Black Fireworks",
+//   //   },
+//   //   (e, r) => {
+//   //     console.log(e, r);
+//   //   }
+//   // );
 
-  // Unlimited3D.getAvailableAnnotations(function (error, result) {
-  //   console.log(result);
-  // });
+//   // Unlimited3D.getAvailableAnnotations(function (error, result) {
+//   //   console.log(result);
+//   // });
 
-  // Unlimited3D.getAvailableMaterials(function (error, result) {
-  //   console.log(result);
-  // });
+//   // Unlimited3D.getAvailableMaterials(function (error, result) {
+//   //   console.log(result);
+//   // });
 
-  // Unlimited3D.getAvailableTextures(function (error, result) {
-  //   console.log(result);
-  // });
+//   // Unlimited3D.getAvailableTextures(function (error, result) {
+//   //   console.log(result);
+//   // });
 
-  // Unlimited3D.getMaterial({ part: "Top 01 Example" }, function (e, r) {
-  //   console.log(e, r);
-  // });
+//   // Unlimited3D.getMaterial({ part: "Top 01 Example" }, function (e, r) {
+//   //   console.log(e, r);
+//   // });
 
-  Unlimited3D.getAvailableParts(function (error, result) {
-    console.log(result);
-  });
+//   Unlimited3D.getAvailableParts(function (error, result) {
+//     console.log(result);
+//   });
 
-  // Unlimited3D.hideParts({
-  //   parts: ['TA003 Yellow Fireworks Black and Black']
-  // })
+//   // Unlimited3D.hideParts({
+//   //   parts: ['TA003 Yellow Fireworks Black and Black']
+//   // })
 
-  // Unlimited3D.getAvailableMaterials((e, r) => {
-  //   console.log(e, 'materiais', r);
-  // });
+//   // Unlimited3D.getAvailableMaterials((e, r) => {
+//   //   console.log(e, 'materiais', r);
+//   // });
 
-  // Unlimited3D.getMaterial(
-  //   {
-  //     part: "TA003 Yellow Fireworks Black and Black:Node-3",
-  //   },
-  //   function (e, r) {
-  //     console.log(e, r);
-  //   }
-  // );
+//   // Unlimited3D.getMaterial(
+//   //   {
+//   //     part: "TA003 Yellow Fireworks Black and Black:Node-3",
+//   //   },
+//   //   function (e, r) {
+//   //     console.log(e, r);
+//   //   }
+//   // );
   
-  // Unlimited3D.applyMaterialToAllParts({ material: "Yellow Fireworks" }, (e) => {
-  //   if (e) console.log(e);
-  // });
-  // Unlimited3D.getAvailableTextures((e, r) => {
-  //   console.log('texturas', e, r);
-  // });
-  // Unlimited3D.changeMaterialColor({
-  //   material: "initialShadingGroup-1",
-  //   color: "#ff0000",
-  // });
-});
+//   // Unlimited3D.applyMaterialToAllParts({ material: "Yellow Fireworks" }, (e) => {
+//   //   if (e) console.log(e);
+//   // });
+//   // Unlimited3D.getAvailableTextures((e, r) => {
+//   //   console.log('texturas', e, r);
+//   // });
+//   // Unlimited3D.changeMaterialColor({
+//   //   material: "initialShadingGroup-1",
+//   //   color: "#ff0000",
+//   // });
+// });
 
 
 
