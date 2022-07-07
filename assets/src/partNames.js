8f320bd7e0c9466f0f405f479ca405cbe993afd9
antonio272a <antoniofelipe2001@hotmail.com>
2022-07-06 22:39:42 -0300

    up

---
+++ assets/src/partNames.js
@@ -0,0 +1,17 @@
+const solutionIdReference = {
+  42476: {
+    name: "PA001 TA",
+    "[top](01)": "TA001",
+    "[top](02){01}": "TA002-SLEEVES",
+    "[subpart][top](02){01}": "TA002-LINE",
+    "[top](03){01}": "TA003",
+    "[bottom](01)": "PA001",
+  },
+  42478: {
+    name: "PA001 TB",
+    "[top](01)": "TB001",
+    "[top](02){01}": "TB002",
+    "[top](03){01}": "TB003-SLEEVES",
+    "[bottom](01)": "PA001",
+  },
+};
--- assets/src/script.js
+++ assets/src/script.js
@@ -43,24 +43,6 @@ window.onload = async () => {
     },
   };
 
-  const solutionIdReference = {
-    42476: {
-      name: "PA001 TA",
-      "[top](01)": "TA001",
-      "[top](02){01}": "TA002-SLEEVES",
-      "[subpart][top](02){01}": "TA002-LINE",
-      "[top](03){01}": "TA003",
-      "[bottom](01)": "PA001",
-    },
-    42478: {
-      name: "PA001 TB",
-      "[top](01)": "TB001",
-      "[top](02){01}": "TB002",
-      "[top](03){01}": "TB003-SLEEVES",
-      "[bottom](01)": "PA001",
-    },
-  };
-
   //  42494; 42594 42489
 
   // top(02) 42476
